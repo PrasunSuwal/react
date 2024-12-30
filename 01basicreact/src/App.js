5515e9e1c1522fd1b1e265fc9ddf6526f41656c7
Prasun Suwal <prasun.suwal@gmail.com>
2024-12-30 09:09:12 +0545

    react start

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,8 @@
+
+function App() {
+  return (
+    <h1>React|Prasun Suwal</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import Hello from "./Hello"
+function App() {
+
+  return (
+    <>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, aspernatur!</p>
+    <Hello/>
+    </>
+  )
+}
+
+export default App
