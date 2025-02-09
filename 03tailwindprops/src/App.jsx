7eed4ca5f42ae96464967f59da337e90b266ef0b
Prasun Suwal <prasun.suwal@gmail.com>
2025-02-09 23:17:06 +0545

    tailwind and props

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Card from './components/card'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='bg-green-300 text-black p-4 rounded-xl mb-5'>Tailwind Test</h1>
+      <Card username="Bored ape"/>
+      <Card username="Copy"/>
+      
+    </>
+        
+    
+  )
+}
+
+export default App
